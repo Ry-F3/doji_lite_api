8c4f0b7457670533e0d08e1ece14489dc4c7aca0
Rhys Few <rhysfeew@gmail.com>
2024-07-09 17:39:13 +0000

    Add trades app, with initial model, serializer and url files.

--- doji_lite_api/urls.py
+++ doji_lite_api/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
     path('', include('profiles.urls')),
+    path('', include('profiles.urls')),
 ]
---
+++ trades/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Trade(models.Model):
+    LONG_SHORT_CHOICES = [
+        ('long', 'long'),
+        ('short', 'short'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='trades')
+    symbol = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    long_short = models.CharField(max_length=5, choices =LONG_SHORT_CHOICES, default='long')
+    margin = models.DecimalField(max_digits=10, decimal_places=2)
+    leverage = models.DecimalField(max_digits=5, decimal_places=3, default=1)
+    entry_price = models.CharField(max_digits=10, decimal_places=2)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    return_pnl = models.DecimalField(max_digits=10, decimal_places=2)
+    is_trade_closed = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('user', 'symbol', 'is_trade_closed')
+
+    def __str__(self):
+        return f'Trade({self.user.username}, {self.symbol}, {self.long_short})'
+
+
+
