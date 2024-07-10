2157d3d5930fe78de9057fbcadb7e0cdccac0506
Rhys Few <rhysfeew@gmail.com>
2024-07-10 13:33:02 +0000

    Added error checking for TradesLIstView. Status code HTTP_404_NOT_FOUND, error 'No Trades Exist'.

--- trades/views.py
+++ trades/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from rest_framework import generics, serializers
+from rest_framework import generics, status, serializers
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from trades.models import Trade
 from .serializers import (TradesSerializer)
 
 class TradesListView(generics.ListAPIView):
     queryset = Trade.objects.all()
     serializer_class = TradesSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if not queryset.exists():
+            return Response(
+                {"error": "No Trades Exist"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+        
