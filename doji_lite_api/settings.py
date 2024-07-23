a5d619f853100e6fd2eb7942429470411f813ef1
Rhys Few <rhysfeew@gmail.com>
2024-07-23 13:10:20 +0000

    Update REGEXES.

--- doji_lite_api/settings.py
+++ doji_lite_api/settings.py
@@ -98,7 +98,7 @@ INSTALLED_APPS = [
     'profiles',
     'trades',
     'historical_datasets',
-    
+
     'pnls',
 ]
 
@@ -124,7 +124,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        r"^https://.*\.codeinstitute-ide\.net$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
