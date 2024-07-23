c810f8461adf4eb79cfa87e2e48da293d96c1db8
Rhys Few <rhysfeew@gmail.com>
2024-07-23 15:56:17 +0000

    Update settings.

--- doji_lite_api/settings.py
+++ doji_lite_api/settings.py
@@ -48,7 +48,7 @@ REST_AUTH_SERIALIZERS = {
 }
 
 REST_USE_JWT = True  # enable token auth
-JWT_AUTH_SECURE = True  # sent over https only
+JWT_AUTH_SECURE = False  # sent over https only
 JWT_AUTH_COOKIE = 'my-app-auth'  # declare cookie name access
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'  # declare cookie name refresh
 JWT_AUTH_SAMESITE = 'None'
