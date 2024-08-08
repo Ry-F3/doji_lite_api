b4c815014b3147cbd9516e50f30f900f59e881c5
Ry-F3 <rhysfeew@gmail.com>
2024-08-08 12:32:35 +0000

    Update gitignore.

--- trade_csv/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UploadFileView, CsvTradeView
-
-urlpatterns = [
-    path('upload1/', UploadFileView.as_view(), name='upload-file'),
-    path('trades-csv1/', CsvTradeView.as_view(), name='csv-trade'),
-]
